94b0a3967b268f990487b81567a23add4483f16a
Blessed <blessedemoefe1122334455@gmail.com>
2025-07-27 18:01:06 +0100

    made updates to the project

--- src/components/Listings/Listings.component.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import {ListingsContainer} from "./Listings.styles"
-
-const Listings = () => {
-  return <ListingsContainer>Listings</ListingsContainer>;
-}
-
-export default Listings                  
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import { HomePageContainer} from "./HomePage.styles";
 import Header from '../../components/header/header.component';
-import Listings from '../../components/Listings/Listings.component';
 import Footer from '../../components/Footer/Footer.component';
 
 function HomePage() {
   return (
     <HomePageContainer>
       <Header />
-     <Listings />
-     <Footer />
     </HomePageContainer>
   );
 }
